f9cfaef00b4e5463b1477426c5884c9b2c7c489f
HasanHira <qubitgraphix@gmail.com>
2023-02-03 17:04:56 +0600

    Operators example

--- practice_01/operators.php
+++ practice_01/operators.php
@@ -1,18 +1,48 @@
 <?php
-// assign operators
+// arithmatic operators
+$x = 12;
+$y = 5;
+$z = $x + $y; // $z = 12 + 5 // arithmatic operator signs + - * / %
+echo $z, "\n";
+// assignment operators
 $num = 5;
 $num = $num + 1;
 echo "\n";
 echo $num;
 echo "\n";
+echo "\n";
 
-$num += 1; // $num = $num + 1 // assign --- // also use in + - * / %
-$num++; // $num + 1 // assign // also in + - * 
-echo $num;
+$number = 3; // $number = $number + 1 // assignment ( = ) // also use in + - * / %
+$number += 1; // $number = $number + 1 // assignment ( = ) // also use in + - * / %
+
+$number++; // $number + 1 // increment operator ++
+$number--; // $number + 1 // decrement operator --
+echo $number;
 
 echo "\n";
 
-$x = 4;
-$x /=2;
-echo $x;
+echo "The signs (+ - * etc.) called operator. OPERATOR & OPERAND complete the operation.", "\n";
+echo "\n";
+
+// some new
+#01.
+$a = $b = $c = 7; // means $a = 7, $b = 7 also $c = 7; 
+echo "{$b}\n";
+echo "\n";
+
+#02. (post-increment)
+$n = 7;
+$m = $n++; // means $m = $n then $n = $n + 1; // ($m = $n).. $n = $n++
+// $x = 4;
+// $x /=2;
+echo "m is: {$m}"." and n is: {$n}"."\n";
+echo "\n";
+
+#03. (pre-increment)
+$p = 7;
+$q = ++$p; // means, first $p = $p + 1 then $q = $p; // $p = $p++ ..($q = $p)
+// $x = 4;
+// $x /=2;
+echo "q is: {$q} and p is: {$p}\n";
+echo "\n";
 
