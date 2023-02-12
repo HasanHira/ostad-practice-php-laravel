f8467453b68e155db32b790b0e812b98c9ad2832
HasanHira <qubitgraphix@gmail.com>
2023-02-12 19:57:57 +0600

    create func file.

---
+++ practice_03/function.php
@@ -0,0 +1,9 @@
+<?php
+#01
+function addTwoNumber(){
+    $num1= 15;
+    $num2 =5;
+    $res= $num1+$num2;
+    echo $res;
+}
+addTwoNumber();
---
+++ practice_03/ztemp.php
@@ -0,0 +1,8 @@
+<?php
+//Write and Describe any (two) 2 stages of the SDLC model?
+
+/* What is Agile Model? Write a short description of it.
+*/
+
+echo "hello world.";
+?>
