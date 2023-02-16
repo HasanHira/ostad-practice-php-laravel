e0d6b18d78b2920059e4cd29e14790f3c7939f15
HasanHira <qubitgraphix@gmail.com>
2023-02-17 02:14:53 +0600

    Fun with associative array

--- practice_04/array-associative.php
+++ practice_04/array-associative.php
@@ -1,9 +1,9 @@
 <?php
 $motorBikePrice = array(
-    "Kawasaki"=> 500000,
-    "FZS3"=>320000,
-    "TVS"=>190000,
-    "FZS2"=>230000,
-    "Palser"=>210000
+    "Kawasaki" => 500000,
+    "FZS3"     => 320000,
+    "TVS"      => 190000,
+    "FZS2"     => 230000,
+    "Palser"   => 210000,
 );
 echo $motorBikePrice["Palser"];
